0a0acb613ed10a1149ed83a79296b9173917200b
AamilS <134914964+AamilS@users.noreply.github.com>
2023-05-29 17:49:55 +0530

    Add files via upload

---
+++ dashboard.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width">
+  <title>replit</title>
+  <link href="style-dashboard.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+  <?php 
+    session_start();
+    if($_SESSION['login']!=true)
+    {
+      header('location:login.php');
+    }
+  ?>
+  <div id="menu">
+    <ul>
+      <li style="--i:7;"><a href="#">Home</a></li>
+      <li style="--i:6;"><a href="#">About</a></li>
+      <li style="--i:5;"><a href="#">Services</a></li>
+      <li style="--i:4;"><a href="#">Portfolio</a></li>
+      <li style="--i:3;"><a href="#">Our Team</a></li>
+      <li style="--i:2;"><a href="#">Contact</a></li>
+      <li style="--i:1;"><a href="login.php">Back</a></li>
+      <?php session_destroy(); ?>
+    </ul>
+  </div>
+</body>
+
+</html>
